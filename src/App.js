e38b757e510da6ce9dc6d30ec1bb903650481a7e
akuto aoyama <aaaa.akuto@gmail.com>
2021-04-26 01:36:11 +0900

    [feat]JSON Serverで受け取りだけ成功

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.member);
+    console.log("this.state.member", this.state.member);
     return (
       <div>
         <button onClick={this.getJson}>Get Json</button>
@@ -35,9 +35,13 @@ class App extends React.Component {
   }
 
   getJson = () => {
-    const url = "https://api.myjson.com/bins/159wqn";
+    // const url = "https://api.myjson.com/bins/159wqn";
+    // https://zipcloud.ibsnet.co.jp/api/search
+    // const url = "https://zipcloud.ibsnet.co.jp/api/search?2700032";
+    const url = "http://localhost:3000/member";
 
     axios.get(url).then((res) => {
+      console.log("res.data", res.data);
       this.setState(res.data);
     });
   };
