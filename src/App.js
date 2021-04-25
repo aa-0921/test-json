ba4d572cb2a2b0c9cf1c2e3adaf52506d9c59555
akuto aoyama <aaaa.akuto@gmail.com>
2021-04-26 01:17:08 +0900

    [feat]axiosの追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+// import { render } from "react-dom";
+
+import axios from "axios";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { member: [] };
+  }
+
+  memberList(list) {
+    const memberList = list.map((member, index) => {
+      return (
+        <li>
+          {member.name} {member.age}
+        </li>
+      );
+    });
+
+    return <ul>{memberList}</ul>;
+  }
+
+  render() {
+    console.log(this.state.member);
+    return (
+      <div>
+        <button onClick={this.getJson}>Get Json</button>
+        {this.memberList(this.state.member)}
+      </div>
+    );
+  }
+
+  getJson = () => {
+    const url = "https://api.myjson.com/bins/159wqn";
+
+    axios.get(url).then((res) => {
+      this.setState(res.data);
+    });
+  };
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
