b203f6b3934d718b00f778f1990bf4261a5c13c5
akuto aoyama <aaaa.akuto@gmail.com>
2021-04-27 00:24:57 +0900

    [fix]ディレクトリ移動させて表示成功

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-// import logo from './logo.svg';
-import "./App.css";
+// import "./App.css";
+import "./assets/App.css";
 
-import React from "react";
-// import { render } from "react-dom";
-
+import React, { useState } from "react";
 import axios from "axios";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { member: [] };
-  }
+// console.log("this.state.member", this.state.member);
 
-  memberList(list) {
-    const memberList = list.map((member, index) => {
-      return (
-        <li>
-          {member.name} {member.age}
-        </li>
-      );
-    });
+export const App = () => {
+  const [member, setMember] = useState([]);
 
-    return <ul>{memberList}</ul>;
-  }
-
-  render() {
-    console.log("this.state.member", this.state.member);
-    return (
-      <div>
-        <button onClick={this.getJson}>Get Json</button>
-        {this.memberList(this.state.member)}
-      </div>
-    );
-  }
-
-  getJson = () => {
+  const getJson = () => {
     // const url = "https://api.myjson.com/bins/159wqn";
     // https://zipcloud.ibsnet.co.jp/api/search
     // const url = "https://zipcloud.ibsnet.co.jp/api/search?2700032";
@@ -42,30 +17,25 @@ class App extends React.Component {
 
     axios.get(url).then((res) => {
       console.log("res.data", res.data);
-      this.setState(res.data);
+      setMember(res.data);
     });
   };
-}
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+  const memberList = (list) => {
+    const memberList = list.map((member, index) => {
+      return (
+        <li key={index}>
+          {member.name} {member.age}
+        </li>
+      );
+    });
+    return <ul>{memberList}</ul>;
+  };
 
-export default App;
+  return (
+    <div>
+      <button onClick={getJson}>Get Json</button>
+      {memberList(member)}
+    </div>
+  );
+};
