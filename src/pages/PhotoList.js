10fa076115c5ede9729116580fb52cd526aed0f6
akuto aoyama <aaaa.akuto@gmail.com>
2021-05-01 06:30:16 +0900

    [feat]Accordionの追加

--- src/pages/PhotoList.js
+++ src/pages/PhotoList.js
@@ -1,7 +1,14 @@
 import "../assets/App.css";
 
 import React, { useState, useEffect } from "react";
-import { Button, CardColumns, Card, Form, Image } from "react-bootstrap";
+import {
+  Button,
+  CardColumns,
+  Card,
+  Form,
+  Image,
+  Accordion,
+} from "react-bootstrap";
 import { Tags } from "../components/Tags";
 
 export const PhotoList = () => {
@@ -40,49 +47,63 @@ export const PhotoList = () => {
 
   return (
     <div className="App">
-      <div className="main">
-        <Form onSubmit={onSubmit}>
-          <Form.Control
-            size="lg"
-            type="text"
-            onChange={(e) => setText(e.target.value)}
-            value={text}
-            placeholder="Large text"
-          />
-          <Button type="submit">Search</Button>
-        </Form>
-      </div>
       <div className="container">
+        <div className="main">
+          <Form onSubmit={onSubmit}>
+            <Form.Control
+              size="lg"
+              type="text"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+              placeholder="Search text"
+            />
+            <Button type="submit">Search</Button>
+          </Form>
+        </div>
         <CardColumns>
           {images.map((image) => (
             <div key={image.id}>
               <Card>
                 <Card.Img variant="top" src={image.urls.regular} />
-                <Card.Body>
-                  <Card.Title>{image.alt_description}</Card.Title>
-                  <Card.Text>
-                    <Tags image={image} />
-                  </Card.Text>
+                <Accordion>
+                  <Card>
+                    <Card.Header>
+                      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Click me!
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        <Card.Title>{image.alt_description}</Card.Title>
+                        <Card.Text>
+                          <Tags image={image} />
+                        </Card.Text>
 
-                  <Card.Text>{image.likes}</Card.Text>
-                  <Card.Text>
-                    <a href={image.links.html}>Link Unsplash</a>
-                  </Card.Text>
+                        <Card.Text>{image.likes}</Card.Text>
+                        <Card.Text>
+                          <a href={image.links.html}>Link Unsplash</a>
+                        </Card.Text>
 
-                  <div className="flex items-center">
-                    <Image
-                      src={image.user.profile_image.large}
-                      rounded
-                      className="w-16 h-16"
-                    />
-                    <p className="text-center py-4 px-4"> {image.user.name}</p>
-                  </div>
+                        <div className="flex items-center">
+                          <Image
+                            src={image.user.profile_image.large}
+                            rounded
+                            className="w-16 h-16"
+                          />
+                          <p className="text-center py-4 px-4">
+                            {" "}
+                            {image.user.name}
+                          </p>
+                        </div>
 
-                  <Card.Text>{image.user.bio}</Card.Text>
-                  <Card.Text>
-                    <a href={image.user.portfolio_url}>portfolio url</a>
-                  </Card.Text>
-                </Card.Body>
+                        <Card.Text>{image.user.bio}</Card.Text>
+                        <Card.Text>
+                          <a href={image.user.portfolio_url}>portfolio url</a>
+                        </Card.Text>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
               </Card>
             </div>
           ))}
