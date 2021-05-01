036e9b53d2ff94e6c5c50c907030592e904392aa
akuto aoyama <aaaa.akuto@gmail.com>
2021-05-02 01:30:29 +0900

    [fix]調整したtoast表示成功

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,13 +5,31 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const Toast = () => {
   const notify = () => {
-    toast("Wow so easy !");
+    toast("🦄 Wow so easy!", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
 
   return (
     <div>
       <button onClick={notify}>Notify !</button>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
